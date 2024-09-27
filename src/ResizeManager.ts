28db904a03b6232eadfbf4787cc38eadb9e3b977
stanislavbonev <stanislav.bonev@gmail.com>
2024-09-27 18:50:02 +0300

    Update ResizeManager.ts

--- src/ResizeManager.ts
+++ src/ResizeManager.ts
@@ -2,15 +2,19 @@ import { Container } from "pixi.js"
 import * as PIXI from 'pixi.js';
 
     export const resizeContainer = (container:Container) => {
-
     const calculatorView = container.children[0] as PIXI.Container
+    let model:string = '';
+
+    if(calculatorView.children[0]) {
+        model = calculatorView.children[0].name
+    }
 
         if(calculatorView.children.length > 0) {
     
              if(!isLandscape) {
-                const scaleX =    window.innerWidth / 260;
-                const scaleY =    window.innerHeight / 485;
-                const scale = Math.min(scaleX, scaleY)*0.8;
+                const scaleX =  window.innerWidth / 260;
+                const scaleY =  window.innerHeight / 485;
+                const scale = Math.min(scaleX, scaleY) * 0.8;
                 container.scale.set(scale);
 
                 container.x =  window.innerWidth  / 2;
@@ -18,7 +22,7 @@ import * as PIXI from 'pixi.js';
             }else {
                 const scaleX = window.innerWidth / 260;
                 const scaleY = window.innerHeight / 485;
-                const scale = Math.min(scaleX, scaleY)*0.8;
+                const scale = Math.min(scaleX, scaleY) * 0.8;
                 container.scale.set(scale);
 
                 container.x =  window.innerWidth  / 2;
